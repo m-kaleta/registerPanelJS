0551c24780cfcee5fead10587bc32254c55fb00f
Mateusz Kaleta <m.kaleta9696@gmail.com>
2020-07-09 17:10:50 +0200

    created registerPanel / added styleCSS

---
+++ script.js
@@ -0,0 +1,7 @@
+const userName = document.querySelector("#username")
+const pass = document.querySelector("#password")
+const pass2 = document.querySelector("#password2")
+const email = document.querySelector("#email")
+
+const sendBtn = document.querySelector(".send")
+const clearBtn = document.querySelector(".clear")
